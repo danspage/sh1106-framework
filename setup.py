b6b89d53e5d199fc2eb1a163ac328563cc141037
Dan Convey <daniel_convey@uri.edu>
2024-05-26 19:53:40 -0400

    Updated version #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sh1106_framework',
-    version='0.0.16',
+    version='0.0.17',
     author='Dan Convey',
     description='A state manager, graphics, image, and custom font drawing package for the SH1106 OLED screen based on the luma.oled package. It works with Raspberry Pi and other Linux-based single-board computers.',
     classifiers=[
